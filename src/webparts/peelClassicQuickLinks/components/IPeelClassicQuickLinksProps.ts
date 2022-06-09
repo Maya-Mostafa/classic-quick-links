ca96c3afda90434b1ed3a7573e96dfe38f538099
Maya-Mostafa <mayamostafa@gmail.com>
2022-06-09 14:15:04 -0400

    solution created

---
+++ src/webparts/peelClassicQuickLinks/components/IPeelClassicQuickLinksProps.ts
@@ -0,0 +1,3 @@
+export interface IPeelClassicQuickLinksProps {
+  description: string;
+}
---
+++ src/webparts/peelClassicQuickLinks/loc/mystrings.d.ts
@@ -0,0 +1,10 @@
+declare interface IPeelClassicQuickLinksWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+}
+
+declare module 'PeelClassicQuickLinksWebPartStrings' {
+  const strings: IPeelClassicQuickLinksWebPartStrings;
+  export = strings;
+}
